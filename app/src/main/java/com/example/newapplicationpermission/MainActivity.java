cf6c8b9246910a3d8962582e399128b9b4ddc49f
Filipe Negrello <crisnello@gmail.com>
2020-07-02 09:24:50 -0300

    Para funcionar em todos android's

--- app/src/main/java/com/example/newapplicationpermission/MainActivity.java
+++ app/src/main/java/com/example/newapplicationpermission/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.newapplicationpermission;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
-
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,9 +46,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getDrawOverOtherApp() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !canDrawOverlaysAfterUserWasAskedToEnableIt(this)) {
             Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
             startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE);
+        }else{
+            tvMsg.setText(getResources().getString(R.string.text_permission_ok));
         }
     }
 
@@ -54,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 String strResp = null;
-                if (Settings.canDrawOverlays(this)) {
+                if (canDrawOverlaysAfterUserWasAskedToEnableIt(this)) {
                     Toast.makeText(this, getResources().getString(R.string.text_permission_ok), Toast.LENGTH_LONG).show();
                     strResp = getResources().getString(R.string.text_permission_ok);
                     btnOpen.setVisibility(View.GONE);
@@ -67,6 +72,50 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public static boolean canDrawOverlaysAfterUserWasAskedToEnableIt(Context context) {
+        if(context == null )
+            return false;
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        } else {
+            if (Settings.canDrawOverlays(context)) {
+                return true;
+            }
+            try {
+                final WindowManager mgr = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+                if (mgr == null) {
+                    return false;
+                }
+                final View viewToAdd = new View(context);
+
+
+                WindowManager.LayoutParams params = new WindowManager.LayoutParams(0, 0, android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ?
+                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY : WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSPARENT);
+                viewToAdd.setLayoutParams(params);
+
+
+                mgr.addView(viewToAdd, params);
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (viewToAdd != null && mgr != null) {
+                            mgr.removeView(viewToAdd);
+                        }
+                    }
+                }, 500);
+
+                return true;
+
+            } catch (Exception e) {
+                return false;
+            }
         }
     }
 
